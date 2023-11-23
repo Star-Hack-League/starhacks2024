1f7b5b8394425b7ce328f2103b1993b85d75d07c
DinosaurPotato534 <shaany0418@gmail.com>
2023-11-23 15:18:11 -0500

    re-add navbar

--- SHL-2024/src/components/Landing.tsx
+++ SHL-2024/src/components/Landing.tsx
@@ -3,6 +3,16 @@ import "../styles/Landing.css";
 function Landing() {
   return (
     <>
+	<div className="navbar-container">
+        <h1 className="navbar">Star Hack Alliance</h1>
+        <div className="navbar-links">
+          <p className="navbar-secondary home">Home</p>
+          <p className="navbar-secondary info">Info</p>
+          <p className="navbar-secondary info">Rankings</p>
+          <p className="navbar-secondary info">Partner</p>
+          <p className="navbar-secondary info events">Events</p>
+        </div>
+      </div>
       <div className="container">
         <div className="centered-content">
           <div className="image-container">
