18962b306938e4d85978dda4f8cb4ac68fb95fa3
DinosaurPotato534 <shaany0418@gmail.com>
2023-11-23 15:25:21 -0500

    fix navbar links

--- SHL-2024/src/components/Landing.tsx
+++ SHL-2024/src/components/Landing.tsx
@@ -3,14 +3,29 @@ import "../styles/Landing.css";
 function Landing() {
   return (
     <>
-	<div className="navbar-container">
-        <h1 className="navbar">Star Hack Alliance</h1>
+      <div className="navbar-container">
+        <a href="https://starhack.tech">
+          <h1 className="navbar">Star Hack Alliance</h1>
+        </a>
         <div className="navbar-links">
-          <p className="navbar-secondary home">Home</p>
-          <p className="navbar-secondary info">Info</p>
-          <p className="navbar-secondary info">Rankings</p>
-          <p className="navbar-secondary info">Partner</p>
-          <p className="navbar-secondary info events">Events</p>
+          <a href="https://starhack.tech" className="link">
+            <p className="navbar-secondary home">Home</p>
+          </a>
+          <a href="https://starhack.tech/info" className="link">
+            <p className="navbar-secondary info">Info</p>
+          </a>
+          <a href="https://starhack.tech/rankings" className="link">
+            <p className="navbar-secondary info">Rankings</p>
+          </a>
+          <a
+            href="https://lll2cu7fgqm.typeform.com/to/Z2Q4Zo7N"
+            className="link"
+          >
+            <p className="navbar-secondary info">Partner</p>
+          </a>
+          <a href="https://starhack.tech/events" className="link">
+            <p className="navbar-secondary info events">Events</p>
+          </a>
         </div>
       </div>
       <div className="container">
